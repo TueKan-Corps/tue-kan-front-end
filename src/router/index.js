28e71f05d99a88d86e1d5b23a3836473185988f2
DrZin-dev <52484219+DrZIn-dev@users.noreply.github.com>
2020-02-08 08:22:24 +0700

    init react router

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Home from "../pages/home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+export default () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Switch>
+        <Route path="/about">
+          <Home></Home>
+        </Route>
+        <Route path="/users">
+          <Home></Home>
+        </Route>
+        <Route path="/"></Route>
+      </Switch>
+    </Router>
+  );
+};
