231387ac596461dcf1bec64364ddcb0779e2e0dd
Boat <werawit_boat@windowslive.com>
2020-03-10 21:14:20 +0700

    commit to checkout

---
+++ src/pages/mainDiv.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const MainDiv = styled.div`
+  margin-top: 60px;
+  overflow: auto;
+  font-family: 'Quicksand', sans-serif;
+  display: grid;
+  grid-template-rows:    1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+`;
+
+export default MainDiv;
---
+++ src/pages/newCreatePost/category.js
@@ -0,0 +1,32 @@
+export const category = [
+  {
+    id: 0,
+    name: '--Select--',
+    value: '',
+  },
+  {
+    id: 1,
+    name: 'Math',
+    value: 0,
+  },
+  {
+    id: 2,
+    name: 'Science',
+    value: 1,
+  },
+  {
+    id: 3,
+    name: 'Thai',
+    value: 2,
+  },
+  {
+    id: 4,
+    name: 'Social',
+    value: 3,
+  },
+  {
+    id: 5,
+    name: 'Eng',
+    value: 4,
+  },
+];
