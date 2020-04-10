28316b5409c1bb2dedb1c4a369d561bb280498b9
boat <werawit_boat@windowslive.com>
2020-04-10 20:35:30 +0700

    edit mainList can get picture

--- src/components/SubContainer/Postlist/Post/index.js
+++ src/components/SubContainer/Postlist/Post/index.js
@@ -17,10 +17,11 @@ export default function Post(props) {
     let linkTo = props.linkTo;
     linkTo = `${linkTo}/${props.postId + 1}`;
     //console.log(props.tag_id);
+    let imgSrc = `https://tue-kan.herokuapp.com/account/${props.account_id}/img`;
     return (
         <Link className='post-list-link' to={linkTo}>
           <div className="post">
-              <img src={props.img} className="img-post" alt='post-list-tutor-img'/>
+              <img src={imgSrc} className="img-post" alt='post-list-tutor-img'/>
               <div className="detail">
                   <div className="tag-subject"><span className="dot" style={{ background : `${colorTag[props.tag_id]}` }}/><h2 className="subject">{subject[props.tag_id]}</h2></div>
                   <div className="topic">#Topic       : {props.topic}</div>
--- src/components/avatar/accountData.js
+++ src/components/avatar/accountData.js
@@ -3,6 +3,18 @@
     -- using for centralized account_id.
 */
 
+import React from 'react';
+
+/*const SetAccountData =(props)=> {
+  const [accountId, setAccountId] = React.useState(0);
+
+  setAccountId(props.id);
+  
+  return accountId;
+}
+
+export default SetAccountData;*/
+
 export const accountData = {
-  account_id: 21,
+  account_id: 29,
 }
