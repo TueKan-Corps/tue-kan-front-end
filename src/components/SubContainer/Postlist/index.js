cf9bbb62c6e97db6b07ad7cdb083d219ef1049a8
boat <werawit_boat@windowslive.com>
2020-04-14 15:18:21 +0700

    add component for empty data list.

--- src/components/SubContainer/Postlist/index.js
+++ src/components/SubContainer/Postlist/index.js
@@ -1,18 +1,42 @@
 /*
   .edit 10-Mar-20 [Boat]
     -- edit to pass object from parent instead of import object in this component. for flexible to use.
+  .edit 14-Apr-20 [Boat]
+    -- add component for empty list.
 */
 
 import React from 'react'
+import Alert from 'react-bootstrap/Alert'
+import { Link } from 'react-dom'
+
 import Post from './Post/index'
 
 export default function Postlist(props) {
   let postData = props.postData;
-    return (
-        <div className="post-list">
-            {postData.map((data, index) => (
-                <Post key={data.id} postId={index} {...data} linkTo={props.linkTo}/>
-            ))}
-        </div>
-    )
+  let isEmpty = postData.length === 0;
+  let emptyLinkTo = props.linkTo === '/ticket' ? '/' : '/createPost';
+  
+  return (
+    <div className="post-list">
+    {
+      /// check length of postData. if isn't empty -> show dataList.
+      /// else -> show text 'no items'.
+      isEmpty !== true ?
+      postData.map((data, index) => (
+        <Post key={data.id} postId={index} {...data} linkTo={props.linkTo}/>
+      ))
+      :
+      <Alert className='posting-empty-alert' variant="success">
+        <Alert.Heading>Hey, nice to see you.</Alert.Heading>
+        <p>
+            You haven't any item now.
+        </p>
+        <hr />
+        <a className="mb-0" href={emptyLinkTo}>
+          You can see items list or create here.
+        </a>
+      </Alert> 
+    }
+    </div>
+  )
 }
