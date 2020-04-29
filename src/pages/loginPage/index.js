b0def6fd9070ede34c49b5eeea7de257ea9b2441
m1crochip <61010416@kmitl.ac.th>
2020-04-29 19:28:23 +0700

    asdf

--- src/pages/loginPage/index.js
+++ src/pages/loginPage/index.js
@@ -252,8 +252,8 @@ export default class Login extends Component {
     }
 
     checkData (result) {
-        //console.log('result here');
-        //console.log(this.state.responseData.account_id);
+        console.log('result here');
+        console.log(this.state.responseData.account_id);
         accountAccess().clearAccountId();
         let checkId = 36;
         if (this.state.responseData.account_id === undefined) {
@@ -286,6 +286,22 @@ export default class Login extends Component {
         return (
             <div className ="loginpage">
                 <div className="container-login" id="container">
+                <div className="form-container sign-in-container">
+                        <form className="form-login" action="#" onSubmit = {this.onLoginSubmit}>
+                            <h1 className="header1-login">Sign in</h1>
+                            <div className="social-container">
+                                <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                                <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                                <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                            </div>
+                            <span className="span-login">or use your account</span>
+                            <input className="input-login form-control" type="username" placeholder="Email" id="usernameLogin" name="usernameLogin" onChange={this.onInputChange} />
+                            <input className="input-login form-control" type="password" placeholder="Password" id="passwordLogin" name="passwordLogin" value={this.state.passwordLogin} onChange={this.onInputChange} />
+                            <a href="#" className="social">Forgot your password?</a>
+                            <button className="button-login">Sign In</button>
+                        </form>
+                    </div>
+
                     <div className="form-container sign-up-container">
                         <form className="form-login" action="#" onSubmit={this.onFormSubmit}>
                             <h1 className="header1-login">Create Account</h1>
@@ -310,21 +326,7 @@ export default class Login extends Component {
                         </form>
 
                     </div>
-                    <div className="form-container sign-in-container">
-                        <form className="form-login" action="#" onSubmit = {this.onLoginSubmit}>
-                            <h1 className="header1-login">Sign in</h1>
-                            <div className="social-container">
-                                <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                                <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                                <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-                            </div>
-                            <span className="span-login">or use your account</span>
-                            <input className="input-login form-control" type="username" placeholder="Email" id="usernameLogin" name="usernameLogin" onChange={this.onInputChange} />
-                            <input className="input-login form-control" type="password" placeholder="Password" id="passwordLogin" name="passwordLogin" value={this.state.passwordLogin} onChange={this.onInputChange} />
-                            <a href="#" className="social">Forgot your password?</a>
-                            <button className="button-login">Sign In</button>
-                        </form>
-                    </div>
+                    
                     <div className="overlay-container">
                         <div className="overlay">
                             <div id="signInContainer" className="overlay-panel overlay-left">
