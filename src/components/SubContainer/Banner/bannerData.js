be002e74f9c05d56eecfac67f01882e9ed3bdd5c
boat <werawit_boat@windowslive.com>
2020-05-05 19:47:25 +0700

    update banner 05-May-20

--- src/components/SubContainer/Banner/bannerData.js
+++ src/components/SubContainer/Banner/bannerData.js
@@ -1,17 +1,27 @@
 export const bannerData = [
     {
         id: 1,
-        src: require('../../../assets/banners/download.jpg'),
+        src: require('../../../assets/banners/download1.jpg'),
         link: "https://google.com"
     },
     {
         id: 2,
-        src: require('../../../assets/banners/download1.jpg'),
+        src: require('../../../assets/banners/download2.jpg'),
         link: "https://google.com"
     },
     {
         id: 3,
         src: require('../../../assets/banners/download3.jpg'),
+        link: "https://google.com"
+    },
+    {
+        id: 4,
+        src: require('../../../assets/banners/download4.jpg'),
+        link: "https://google.com"
+    },
+    {
+        id: 5,
+        src: require('../../../assets/banners/download5.jpg'),
         link: "https://google.com"
     },
 ]
